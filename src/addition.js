ed7906c142c343c4dbdd57680a73c16f47890249
Jenil Pipada <jenil.p194@gmail.com>
2025-10-08 21:56:17 +0530

    tests for custom delimiter

--- src/addition.js
+++ src/addition.js
@@ -3,9 +3,36 @@ function add(str){
         return 0;
     }
 
-    const nums = str.split(/[,\n]+/); // split by comma or semicolon
+    var delimiter = ","
+    var new_str = str;
+
+// check for custom delimiter
+
+     if (str.startsWith('//')) {
+    const line_change = str.indexOf('\n');
+    if (line_change === -1) {
+      throw new Error('Please provide valid input - no newline found after custom delimiter declaration');
+    }
+
+
+    delimiter = str.substring(2, line_change);
+    new_str = str.substring(line_change + 1);
+}
+
+
+    
+    console.log(delimiter);
+
+    const nums = new_str.split(new RegExp(`${escapeRegExp(delimiter)}|\\r?\\n`)); // split by comma or semicolon
+
+    console.log(nums);
     
     return nums.reduce((res, num) => {return res + Number(num)}, 0);
+}
+
+
+function escapeRegExp(str) {
+ return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 }
 
 module.exports = {add};
--- tests/addition.test.js
+++ tests/addition.test.js
@@ -8,5 +8,8 @@ describe("tests for add function", () =>{
     test("sum of multiple comma and new line seperated numbers", () =>{
         expect(add("1\n2,3")).toBe(6);
     })
+
+    test("tests for custom delimiter", () =>{
+        expect(add("//;\n1;2")).toBe(3);})                  
 })
 
