9f1eaae68dd415dd57004c306184e5054dc29f8d
Jenil Pipada <jenil.p194@gmail.com>
2025-10-08 20:42:04 +0530

    sum of multiple comma and new line seperated numbers

--- src/addition.js
+++ src/addition.js
@@ -2,6 +2,10 @@ function add(str){
     if(str === ""){
         return 0;
     }
+
+    const nums = str.split(/[,\n]+/); // split by comma or semicolon
+    
+    return nums.reduce((res, num) => {return res + Number(num)}, 0);
 }
 
 module.exports = {add};
--- tests/addition.test.js
+++ tests/addition.test.js
@@ -4,4 +4,9 @@ describe("tests for add function", () =>{
     test("returns 0 for empty string", () =>{
         expect(add("")).toBe(0);
     });
+
+    test("sum of multiple comma and new line seperated numbers", () =>{
+        expect(add("1\n2,3")).toBe(6);
+    })
 })
+
